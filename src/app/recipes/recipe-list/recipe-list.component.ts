4f3fc3209f4c6a41203a1a807df411a3f93a7ef9
Cook1619 <cook6348@gmail.com>
2019-02-27 10:49:07 -0600

    Adding recipe model to be able to be used whereever a recipe is needed, and started styling recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Is amazing','https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.cicis.com%2Fmedia%2F1176%2Fpizza_trad_pepperonibeef.png&imgrefurl=https%3A%2F%2Fwww.cicis.com%2Fmenu%2Fpizza%2Fpepperoni-beef&docid=fm5r5jRTvhqewM&tbnid=x_7gN_kuIgT94M%3A&vet=10ahUKEwj3iI-frdzgAhUi9IMKHdPHDFIQMwhtKAUwBQ..i&w=1538&h=776&bih=1041&biw=2133&q=pizza&ved=0ahUKEwj3iI-frdzgAhUi9IMKHdPHDFIQMwhtKAUwBQ&iact=mrc&uact=8')
+  ];
   constructor() { }
 
   ngOnInit() {
